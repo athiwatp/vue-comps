f2c134d722a0f8c085fa2bc41f6735172f6f2f95
Hejx <531601727@qq.com>
2016-07-12 22:33:12 +0800

    edit preview app.vue

--- preview/views/app.vue
+++ preview/views/app.vue
@@ -1,51 +1,17 @@
 <template>
     <div class="content">
-        <div class="content-block-title">welcome to vue components.</div>
+        <div class="logo"></div>
+        <h3>Vue Comps</h3>
+        <div class="content-block-title center">welcome to vue components.</div>
         <ul class="list-view">
-            <li>
+            <li v-for="item in lists">
                 <div class="item-content">
                     <div class="item-media">
-                        <i class="icon icon-alert"></i>
+                        <i class="icon {{item.icon}}"></i>
                     </div>
                     <div class="item-inner">
                         <div class="item-title">
-                            Alert
-                        </div>
-                    </div>
-                </div>
-            </li>
-             <li>
-                <div class="item-content">
-                    <div class="item-media">
-                        <i class="icon icon-confirm"></i>
-                    </div>
-                    <div class="item-inner">
-                        <div class="item-title">
-                            Confirm
-                        </div>
-                    </div>
-                </div>
-            </li>
-             <li>
-                <div class="item-content">
-                    <div class="item-media">
-                        <i class="icon icon-message"></i>
-                    </div>
-                    <div class="item-inner">
-                        <div class="item-title">
-                            Toast
-                        </div>
-                    </div>
-                </div>
-            </li>
-             <li>
-                <div class="item-content">
-                    <div class="item-media">
-                        <i class="icon icon-panel"></i>
-                    </div>
-                    <div class="item-inner">
-                        <div class="item-title">
-                           Panel
+                            {{item.name}}
                         </div>
                     </div>
                 </div>
@@ -55,9 +21,61 @@
 </template>
 
 <style type="less">
-    
+    .logo{
+        width: 100%;
+        height: 200px;
+        margin: 20px 0;
+        background: url("../assets/images/hugo.jpg") center center no-repeat;
+        background-size: contain;
+    }
+    h3, .center{
+        text-align: center;
+    }
+    .content-block-title{
+        margin-top: 15px;
+        text-transform: lowercase;
+    }
 </style>
 
 <script>
-    export default {}
+    export default {
+        data(){
+            return{
+                lists: [{
+                    name: 'Alert',
+                    icon: 'icon-alert'
+                },{
+                    name: 'Confirm',
+                    icon: 'icon-confirm'
+                },{
+                    name: 'Toast',
+                    icon: 'icon-message'
+                },{
+                    name: 'Panel',
+                    icon: 'icon-panel'
+                },{
+                    name: 'Vritual List',
+                    icon: 'icon-list'
+                },{
+                    name: 'Pull Down',
+                    icon: 'icon-pulldown'
+                },{
+                    name: 'Loading',
+                    icon: 'icon-loading'
+                },{
+                    name: 'Date',
+                    icon: 'icon-date'
+                },{
+                    name: 'Slider',
+                    icon: 'icon-slider'
+                },{
+                    name: 'Notify',
+                    icon: 'icon-notify'
+                },{
+                    name: 'Grid',
+                    icon: 'icon-grid'
+                }]
+            }
+        }
+    }
 </script>
