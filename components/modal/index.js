e850d36e5c0e1b1222d3a4ef0a65828fb850b1d1
Hejx <531601727@qq.com>
2016-08-03 14:09:18 +0800

    edit modal

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,6 +1,8 @@
 /*======================================================
 ************   Modals   ************
 ======================================================*/
+require('./index.less');
+
 var modalModule = {};
 
 var _modalTemplateTempDiv = document.createElement('div');
@@ -10,22 +12,22 @@ modalModule.modal = function (params) {
     var modalHTML = '', buttonsHTML = '';
     if (params.buttons && params.buttons.length > 0) {
         for (var i = 0; i < params.buttons.length; i++) {
-            buttonsHTML += '<span class="modal-button' + (params.buttons[i].bold ? ' modal-button-bold' : '') + '">' + params.buttons[i].text + '</span>';
+            buttonsHTML += '<span class="modal-m-button' + (params.buttons[i].bold ? ' modal-m-button-bold' : '') + '">' + params.buttons[i].text + '</span>';
         }
     }
-    var titleHTML = params.title ? '<div class="modal-title">' + params.title + '</div>' : '';
-    var textHTML = params.text ? '<div class="modal-text">' + params.text + '</div>' : '';
+    var titleHTML = params.title ? '<div class="modal-m-title">' + params.title + '</div>' : '';
+    var textHTML = params.text ? '<div class="modal-m-text">' + params.text + '</div>' : '';
     var afterTextHTML = params.afterText ? params.afterText : '';
-    var noButtons = !params.buttons || params.buttons.length === 0 ? 'modal-no-buttons' : '';
-    var verticalButtons = params.verticalButtons ? 'modal-buttons-vertical': '';
-    var modalButtonsHTML = params.buttons && params.buttons.length > 0 ? '<div class="modal-buttons modal-buttons-' + params.buttons.length + ' ' + verticalButtons + '">' + buttonsHTML + '</div>' : '';
-    modalHTML = '<div class="modal ' + noButtons + ' ' + (params.cssClass || '') + '"><div class="modal-inner">' + (titleHTML + textHTML + afterTextHTML) + '</div>' + modalButtonsHTML + '</div>';
+    var noButtons = !params.buttons || params.buttons.length === 0 ? 'modal-m-no-buttons' : '';
+    var verticalButtons = params.verticalButtons ? 'modal-m-buttons-vertical': '';
+    var modalButtonsHTML = params.buttons && params.buttons.length > 0 ? '<div class="modal-m-buttons modal-m-buttons-' + params.buttons.length + ' ' + verticalButtons + '">' + buttonsHTML + '</div>' : '';
+    modalHTML = '<div class="modal-m ' + noButtons + ' ' + (params.cssClass || '') + '"><div class="modal-m-inner">' + (titleHTML + textHTML + afterTextHTML) + '</div>' + modalButtonsHTML + '</div>';
 
     _modalTemplateTempDiv.innerHTML = modalHTML;
     document.body.appendChild(_modalTemplateTempDiv.firstChild);
 
     // Add events on buttons
-    var btns = document.body.querySelectorAll('.modal-button');
+    var btns = document.body.querySelectorAll('.modal-m-button');
     for (var i = 0; i < btns.length; i++) {
         (function(index){
             btns[index].addEventListener('click', function (e) {
@@ -48,10 +50,10 @@ modalModule.alert = function (text, title, callbackOk) {
     });
 };
 modalModule.openModal = function () {
-    var modal = document.querySelector('.modal');
+    var modal = document.querySelector('.modal-m');
     var _className = modal.className,
         isModal = false;
-    if (_className.indexOf('modal') > -1) {
+    if (_className.indexOf('modal-m') > -1) {
         isModal = true;
     }
     
@@ -61,23 +63,30 @@ modalModule.openModal = function () {
     }
 
     var overlay = document.createElement('div');
-        overlay.className = 'modal-overlay modal-overlay-visible';
-    if (document.getElementsByClassName('modal-overlay').length === 0) {
+        overlay.className = 'modal-m-overlay modal-m-overlay-visible';
+    if (document.getElementsByClassName('modal-m-overlay').length === 0) {
         document.body.appendChild(overlay);
+    } else {
+        document.getElementsByClassName('modal-m-overlay')[0].className = 'modal-m-overlay modal-m-overlay-visible';
     }
 
-    modal.className = _className.replace('modal-out', '') + 'modal-in';
+    modal.className = _className.replace('modal-m-out', '') + 'modal-m-in';
 };
 modalModule.closeModal = function () {
-    var modal = document.querySelector('.modal'),
+    var modal = document.querySelector('.modal-m'),
         _className = modal.className;
     if (typeof modal !== 'undefined' && modal.length === 0) {
         return;
     }
-    var overlay = document.querySelector('.modal-overlay');
+    var overlay = document.querySelector('.modal-m-overlay');
     
-    overlay.className = 'modal-overlay';
-    modal.className = _className.replace('modal-out', '').replace('modal-in', '');
+    modal.className = _className.replace('modal-m-out', '').replace('modal-m-in', '');
+    modal.addEventListener('webkitTransitionEnd, transitionend', function () {
+        console.log('webkitTransitionEnd');
+        modal.parentNode.removeChild(modal);
+    }, false);
+
+    overlay.className = 'modal-m-overlay';
 };
 
 
