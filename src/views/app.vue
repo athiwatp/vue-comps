2057b0b0cb189d094b1b117e88239ffb6d98784d
Hejx <531601727@qq.com>
2016-08-07 13:29:10 +0800

    update appview

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="content">
-        <h3>Vue Comps</h3>
-        <div class="content-block-title center">welcome to vue components.</div>
-        <ul class="list-view">
-            <li v-for="item in lists">
-                <a href="#{{item.link}}" class="item-link">
-                    <div class="item-content">
-                        <div class="item-media" v-bind:class="{'off': !item.link}">
-                            <i class="icon {{item.icon}}"></i>
-                        </div>
-                        <div class="item-inner">
-                            <div class="item-title">
-                                {{item.name}}
+        <div v-for="list in lists">
+            <div class="content-block-title">{{list.name}}</div>
+            <ul class="list-view">
+                <li v-for="item in list.value">
+                    <a href="#{{item.link}}" class="item-link">
+                        <div class="item-content">
+                            <div class="item-media" v-bind:class="{'off': !item.link}">
+                                <i class="icon {{item.icon}}"></i>
+                            </div>
+                            <div class="item-inner">
+                                <div class="item-title">
+                                    {{item.name}}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-        </ul>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -46,63 +47,78 @@
         data(){
             return{
                 lists: [{
-                    name: 'Alert',
-                    icon: 'icon-alert',
-                    link: '/alert'
+                    name: 'js components',
+                    value: [{
+                        name: 'Alert',
+                        icon: 'icon-alert',
+                        link: '/alert'
+                    },{
+                        name: 'Confirm',
+                        icon: 'icon-confirm',
+                        link: '/confirm'
+                    },{
+                        name: 'Prompt',
+                        icon: 'icon-prompt',
+                        link: '/prompt'
+                    },{
+                        name: 'Action Sheet',
+                        icon: 'icon-actionsheet',
+                        link: '/actionsheet'
+                    },{
+                        name: 'Preloader',
+                        icon: 'icon-loading',
+                        link: '/preloader'
+                    },{
+                        name: 'Toast',
+                        icon: 'icon-toast-1',
+                        link: '/toast'
+                    },{
+                        name: 'Modal',
+                        icon: 'icon-message',
+                        link: '/modal'
+                    },{
+                        name: 'Notify',
+                        icon: 'icon-notify',
+                        link: '/notify'
+                    },{
+                        name: 'Popover',
+                        icon: 'icon-panel',
+                        link: '/popover'
+                    },{
+                        name: 'Infinite Scroll',
+                        icon: 'icon-wuxian'
+                    },{
+                        name: 'Pull Down',
+                        icon: 'icon-pulldown'
+                    },{
+                        name: 'Photo Browser',
+                        icon: 'icon-photo'
+                    },{
+                        name: 'Date',
+                        icon: 'icon-date'
+                    },{
+                        name: 'Slider',
+                        icon: 'icon-slider'
+                    }]
                 },{
-                    name: 'Confirm',
-                    icon: 'icon-confirm',
-                    link: '/confirm'
-                },{
-                    name: 'Prompt',
-                    icon: 'icon-prompt',
-                    link: '/prompt'
-                },{
-                    name: 'Action Sheet',
-                    icon: 'icon-actionsheet',
-                    link: '/actionsheet'
-                },{
-                    name: 'Preloader',
-                    icon: 'icon-loading',
-                    link: '/preloader'
-                },{
-                    name: 'Toast',
-                    icon: 'icon-toast-1',
-                    link: '/toast'
-                },{
-                    name: 'Modal',
-                    icon: 'icon-message',
-                    link: '/modal'
-                },{
-                    name: 'Notify',
-                    icon: 'icon-notify',
-                    link: '/notify'
-                },{
-                    name: 'Popover',
-                    icon: 'icon-panel',
-                    link: '/popover'
-                },{
-                    name: 'Grid',
-                    icon: 'icon-grid',
-                    link: '/grid'
-                },{
-                    name: 'Infinite Scroll',
-                    icon: 'icon-wuxian'
-                },{
-                    name: 'Pull Down',
-                    icon: 'icon-pulldown'
-                },{
-                    name: 'Vritual List',
-                    icon: 'icon-list'
-                },{
-                    name: 'Photo Browser',
-                    icon: 'icon-photo'
-                },{
-                    name: 'Date',
-                    icon: 'icon-date'
-                },{
-                    name: 'Slider',
-                    icon: 'icon-slider'
+                    name: 'base components',
+                    value: [{
+                        name: 'Page',
+                        icon: 'icon-page'
+                    },{
+                        name: 'List',
+                        icon: 'icon-list'
+                    },{
+                        name: 'Grid',
+                        icon: 'icon-grid',
+                        link: '/grid'
+                    },{
+                        name: 'Checkbox',
+                        icon: 'icon-checkbox'
+                    },{
+                        name: 'Radio',
+                        icon: 'icon-radio'
+                    }]
                 }]
             }
         }
